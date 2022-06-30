2fef0491cf7f9d81d42530550ed98afc99d8c031
kontseus <konceus1@gmail.com>
2022-06-30 19:53:24 +0300

    setup

--- Config/configs.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db' => [
-        'host' => getenv('DB_HOST') ?? '',
-        'database' => getenv('DB_NAME') ?? '',
-        'user' => getenv('DB_USER') ?? '',
-        'password' => getenv('DB_PASSWORD') ?? '',
-    ]
-];
--- public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__));
-$dotenv->load();
